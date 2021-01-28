f946e53009561eaa925d856e66778fd4431a3929
konrad2501 <47695588+konrad2501@users.noreply.github.com>
2021-01-28 19:20:58 +0100

    Delete Farm/src/main/java/application/Exhibition directory

--- Farm/src/main/java/application/Exhibition/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package application.Exhibition;
-
-import application.User.User;
-
-import java.util.List;
-
-public interface ProductRepository {
-    void buyProduct(User user, Exhibition exhibition);
-}
